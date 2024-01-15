898ffabcf5cacae84ce5694721be078d746fc49f
Moon-ByeongHak <nate_id@naver.com>
2024-01-15 16:03:58 +0900

    commit

---
+++ src/main/java/com/dayone/scraper/YahooFinanceScraper.java
@@ -0,0 +1,94 @@
+package com.dayone.scraper;
+
+import com.dayone.model.Company;
+import com.dayone.model.Dividend;
+import com.dayone.model.ScrapedResult;
+import com.dayone.model.constants.Month;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class YahooFinanceScraper implements Scraper{
+
+    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1mo";
+    private static final String SUMMARY_URL = "https://finance.yahoo.com/quote/%s?p=%s";
+
+    private static final long START_TIME = 86400; // 60초 * 60분 * 24시간
+
+
+    @Override
+    public ScrapedResult scrap(Company company) {
+        var scrapResult = new ScrapedResult();
+        scrapResult.setCompany(company);
+        try {
+            long now = System.currentTimeMillis() / 1000;
+
+            String url = String.format(STATISTICS_URL, company.getTicker(), START_TIME, now);
+            Connection connection = Jsoup.connect(url);
+            Document document = connection.get();
+
+            // 가져오게될 테이블이 많을 수 있어 Elements 타입으로 받아온다.
+            Elements parsingDivs = document.getElementsByAttributeValue("data-test", "historical-prices");
+            // 이후 테이블 1개만 Element 타입으로 다시 받아오게 된다.
+            Element tableEle = parsingDivs.get(0);
+
+            // 헤드면 0 바디면 1 풋이면 2하면 됨
+            Element tbody = tableEle.children().get(1);
+
+            List<Dividend> dividends = new ArrayList<>();
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+
+                String[] splits = txt.split(" ");
+                int month = Month.strToNumber(splits[0]);
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+
+                if (month < 0) {
+                    throw new RuntimeException("Unexpected Month enum value -> " + splits[0]);
+                }
+
+                dividends.add(new Dividend(LocalDateTime.of(year, month, day, 0, 0), dividend));
+//                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
+            scrapResult.setDividends(dividends);
+
+        } catch (IOException e) {
+            // To Do
+            e.printStackTrace();
+        }
+        return scrapResult;
+    }
+
+
+    @Override
+    public Company scrapCompanyByTicker(String ticker) {
+        String url = String.format(SUMMARY_URL, ticker, ticker);
+
+        try {
+            Document document = Jsoup.connect(url).get();
+            Element titleEle = document.getElementsByTag("h1").get(0);
+            String title = titleEle.text().split(" - ")[1].trim();
+
+            return new Company(ticker, ticker);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
